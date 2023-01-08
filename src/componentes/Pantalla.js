07fc3bd472efa066eb641f5340d6b685edf9ffb0
Feper07 <fernando.caamachoo@gmail.com>
2023-01-08 18:39:34 -0500

    Se agrega el proyecto

---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../hoja-de-estilos/Pantalla.css";
+
+const Pantalla = ({input}) => (
+    <div className="input">
+        {input}
+    </div>
+);
+export default Pantalla;
